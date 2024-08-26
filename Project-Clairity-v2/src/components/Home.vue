d95adc91d0199c3e908bbb11f3235597aa58d42d
marcohartono <marco.hartono@gmail.com>
2024-08-26 22:28:46 +0800

    test

--- Project-Clairity-v2/src/components/Home.vue
+++ Project-Clairity-v2/src/components/Home.vue
@@ -1,4 +1,28 @@
 <template>
-    <h1>Home</h1>
+    <b-row>
+        <b-col>
+            <h2>Be Mindful of Your Workspace</h2>
+            <h1>Project Clarity</h1>
+            <p>Elevate productivity and well-being with cl(air)ity's smart air quality sensors</p>
+            <b-row>
+                <b-col>
+                    <div>
+                        <img src="" alt="">
+                        <h3>
+                            Real-time
+                        </h3>
+                        <p>Deliver real-time data insights</p>
+                    </div>
+                </b-col>
+                <b-col></b-col>
+                <b-col></b-col>
+            </b-row>
+        </b-col>
+        <b-col>
+            <h1>test</h1>
+        </b-col>
+
+    </b-row>
+
 </template>
   
