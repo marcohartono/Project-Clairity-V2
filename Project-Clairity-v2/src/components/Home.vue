ed4cb796753d2f3d4ff347b7b276804825a00c85
marcohartono <marco.hartono@gmail.com>
2024-08-30 18:49:52 +0800

    .env.example

--- Project-Clairity-v2/src/components/Home.vue
+++ Project-Clairity-v2/src/components/Home.vue
@@ -1,28 +1,35 @@
 <template>
-    <b-row>
-        <b-col>
-            <h2>Be Mindful of Your Workspace</h2>
-            <h1>Project Clarity</h1>
-            <p>Elevate productivity and well-being with cl(air)ity's smart air quality sensors</p>
-            <b-row>
-                <b-col>
-                    <div>
-                        <img src="" alt="">
-                        <h3>
-                            Real-time
-                        </h3>
-                        <p>Deliver real-time data insights</p>
-                    </div>
-                </b-col>
-                <b-col></b-col>
-                <b-col></b-col>
-            </b-row>
-        </b-col>
-        <b-col>
-            <h1>test</h1>
-        </b-col>
+    <main>
+        <b-row>
+            <b-col>
+                <h2>Be Mindful of Your Workspace</h2>
+                <h1>Project Clarity</h1>
+                <p>Elevate productivity and well-being with cl(air)ity's smart air quality sensors</p>
+                <b-row>
+                    <b-col>
+                        <div>
+                            <img src="" alt="">
+                            <h3>
+                                Real-time
+                            </h3>
+                            <p>Deliver real-time data insights</p>
+                        </div>
+                    </b-col>
+                    <b-col></b-col>
+                    <b-col></b-col>
+                </b-row>
+            </b-col>
+            <b-col>
+                <h1>test</h1>
+            </b-col>
 
-    </b-row>
+        </b-row>
+    </main>
+   
 
 </template>
+
+<script>
+    
+</script>
   
